8a64c4d9e8df557b69a4e38d441d334247216241
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-01 09:21:34 -0500

    style: :sparkles: Add Style
    The styles in div, form and button.

--- index.php
+++ index.php
@@ -5,34 +5,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <form method="post">
-        <input type="text" name="numero1" placeholder="Ingrese el numero">
-        <select name="select">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
-        <input type="text" name="numero2" placeholder="Ingrese el numero"><br>
-        <input type="submit" value="Enviar">
-    </form>
     <?php
+        $resul = 0;
+        $mensaje = "";
         switch ($_POST['select']) {
             case '+':
-                echo $_POST['numero1'] + $_POST['numero2'];
+                $resul = $_POST['numero1'] + $_POST['numero2'];
+                $mensaje = "{$_POST['numero1']} + {$_POST['numero2']} = $resul";
                 break;
             case '-':
-                echo $_POST['numero1'] - $_POST['numero2'];
+                $resul = $_POST['numero1'] - $_POST['numero2'];
                 break;
             case '*':
-                echo $_POST['numero1'] * $_POST['numero2'];
+                $resul = $_POST['numero1'] * $_POST['numero2'];
                 break;
             case '/':
-                echo $_POST['numero1'] / $_POST['numero2'];
+                if($_POST['numero2'] == 0){
+                    $resul = 'La division no se puede realizar';
+                } else {
+                    $resul = $_POST['numero1'] / $_POST['numero2'];
+                }
                 break;
         }
     ?>
+    <section>
+        <div class="operaciones">
+            <p>
+                <?php echo "$mensaje"?>
+            </p> 
+        </div>
+        <div class="resultado">
+            <p>
+                <?php echo $resul?>
+            </p>
+        </div>
+        <form method="post">
+            <input class="num" type="text" name="numero1" placeholder="Numero 1">
+            <select name="select">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input class="num" type="text" name="numero2" placeholder="Numero 2"><br>
+            <input class="btn-equal" type="submit" value="=">
+        </form>
+    </section>
+    
 </body>
 </html>
